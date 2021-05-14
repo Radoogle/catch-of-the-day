54c2c98b156f5e08e528950ebeb3fe3f0a32e4de
Radu H <52208792+Radoogle@users.noreply.github.com>
2021-05-14 20:07:38 +0300

    #25 & #26 Build React for Production & Deploy to Now/Vercel

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -26,7 +26,7 @@ class Inventory extends React.Component {
             if(user) {
                 this.authHandler({ user });
             }
-        })
+        });
     }
 
     authHandler = async (authData) => {
